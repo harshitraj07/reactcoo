29e7bc0bb47a5cf03e0797410c170fa020a58c73
harshitraj07 <harshitraj0702@gmail.com>
2025-09-06 18:17:11 +0530

    WIP: local changes

--- reactcomponent/src/App.tsx
+++ reactcomponent/src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import Sidebar from './component/sidebar/sidebar';
-// import Header from './component/header/Header'; // ✅ import it
+// import Header from './component/header/Header'; 
 import Home from './pages/dashboard/Home';
 import Profile from './pages/SignUp/SignUp';
-import Settings from './pages/settings/Settings';
 import Signup from './pages/SignUp/SignUp';
 import LandingPage from './pages/landingpage/landingPage';
 const App = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
+  // const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
 
   return (
     <Router>
@@ -18,12 +17,12 @@ const App = () => {
   <Header isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} /> */}
 
 <div
-  style={{
-    marginLeft: isSidebarOpen ? '60px' : '60px',
-    paddingTop: '64px',
-    padding: '1rem',
-    transition: 'margin-left 0.3s ease',
-  }}
+  // style={{
+  //   marginLeft: isSidebarOpen ? '60px' : '60px',
+  //   paddingTop: '64px',
+  //   padding: '1rem',
+  //   transition: 'margin-left 0.3s ease',
+  // }}
 >
         <Routes>
     {/* <Route
